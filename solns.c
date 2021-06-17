73e0d0d161a9242c3afa902863f374bbc16c9ad0
Bhavyasree10 <84732887+Bhavyasree10@users.noreply.github.com>
2021-06-17 16:55:50 +0530

    Update solns.c

--- solns.c
+++ solns.c
@@ -1,2 +1,114 @@
-/* Enter your solutions in this file */
+/* max function */
 #include <stdio.h>
+int main(){
+  int max(int a[],int n){
+    int m=0;
+    for(int i=0;i<n;i++){
+      if(a[i]>m){
+        m=a[i];
+      }
+    }
+    return m;
+  }
+  return 0;
+}
+
+
+
+/*min function*/
+#include <stdio.h>
+int main(){
+  int min(int a[],int n){
+    int m=0;
+    for(int i=0;i<n;i++){
+      if(a[i]<m){
+        m=a[i];
+    }
+    }
+  return m;
+  }
+  return 0;
+}
+
+
+
+/*average function*/
+float average(inta[],int n){ float sum = 0 ;
+     for(int i=0; i<n ; i++){
+       sum = sum + a[i];
+
+    }
+    return sum/n;
+}
+
+
+
+/mode function/
+int mode(int a[], int length){
+   int n = max(a,length);
+   int p = min(a,length);
+   int counts[100],  d=-1;
+       for(int i = p; i <= n ; i++){
+         int c = 0;
+          for ( int j=0; j<length; j++){
+            if(a[j]==i){
+               c++;
+          }
+           
+      }   
+     counts[++d] = c;
+  }
+  
+  int b = 0,f = 0 ;
+  for (int i=0 ; i<=d ; i++){
+    if ( counts[i] > b ){
+       b = counts[i];
+       f = i+p;
+     }
+   }
+    return f;
+}
+
+
+
+/*factor function*/
+int isprime(int n){
+  for (int i = 2; i<n; i++){
+    if(n%i==0){
+       return 0;
+    }
+    else{
+     return 1;
+     }
+  }
+
+}
+
+int factors(int x, int arr[]){
+ int cnt=0;
+   for( int i = 2; i < x; i++){
+      while( x % i == 0){
+                if(isprime(i)){
+                     arr[cnt] = i ;
+                     x = x/i ; 
+                     cnt++;
+                     }
+                          
+                            if (isprime(x)){
+                               arr[cnt] = x;
+                              
+                               
+                           }
+                }
+        
+     }
+   return cnt+1;
+ }
+    
+  
+  
+  
+  
+  
+  
+  
